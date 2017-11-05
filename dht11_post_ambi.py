6b7a141d4c5a9985ca6732d65dde69d56b262430
hujuu <out.of.the.base.sfc@gmail.com>
2017-11-05 21:19:22 +0900

    ambientへのポスト

---
+++ dht11_post_ambi.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+import ambient
+
+# ご自分のチャネルID、ライトキーに置き換えてください
+ambi = ambient.Ambient(2607, "fca0d37c61f9dc95")
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+instance = dht11.DHT11(pin = 4)
+
+while True:
+    result = instance.read()
+    if result.is_valid():
+        print("Last valid input: " + str(datetime.datetime.now()))
+        print("Temperature: %d C" % result.temperature)
+        print("Humidity: %d %%" % result.humidity)
+
+        r = ambi.send({"d1": result.temperature, "d2": result.humidity})
+
+    time.sleep(10)
